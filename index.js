6255b2d8b9aa8cd77abf5d9fb80f3a165e7d59db
SaeedKarimian860 <SGK12190@gmail.com>
2024-11-15 00:56:22 -0500

    Node Command line tools

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fs = require("fs");
 const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
 
 // Method #2
 // const lstat = util.promisify(fs.lstat);
@@ -9,16 +11,28 @@ const util = require("util");
 // Method #3
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-const statPromises = filenames.map(filename => {
-  return lstat(filename);
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
+  }
 });
 
 // Method #1
